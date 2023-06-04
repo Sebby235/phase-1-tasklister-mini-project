70c29f801dccdf2406ee98f031a5c0f832b59f3b
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-06-04 01:09:41 -0500

    todo task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form")
+  const taskDescr = document.getElementById("new-task-description")
+  const taskList = document.getElementById("tasks")
+
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const p = document.createElement('p')
+    const btn = document.createElement('button')
+
+    p.textContent = taskDescr.value
+
+    p.appendChild(btn)
+    taskList.appendChild(p)
+
+    btn.textContent = 'delete'
+
+    btn.addEventListener('click', (e) => {
+      p.remove()
+    })
+  })
+
 });
+
